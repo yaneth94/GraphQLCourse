1121f857a1c1c4554730fa6ea679f20f95df75d6
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-04 10:56:57 -0600

    added hash in password#

--- db/models/user.js
+++ db/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -10,6 +11,21 @@ const userSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "Course",
     }, ],
+});
+
+userSchema.virtual("password");
+//antes de ejecutar las validaciones se hace la encriptacion
+userSchema.pre("validate", async function() {
+    if (this.password === undefined) return;
+
+    try {
+        const hash = await bcrypt.hash(this.password, 10);
+        console.log(hash, this.password);
+        this.hashedPassword = hash;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
 });
 
 module.exports = mongoose.model("User", userSchema);
